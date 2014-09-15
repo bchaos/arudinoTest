6b30f3ff7ad2aa269ca6e71ef988dc271edea77d
bradfordFarmer <brad@propeller-com.com>
2014-09-15 17:43:35 -0400

    updated server code

--- js/server.js
+++ js/server.js
@@ -16,10 +16,14 @@
     return fs.readFile(__dirname + '/index.html', function(err, data) {
       if (err) {
         res.writeHead(500);
+            console.log('error');
         return res.end('error loading index.html');
+         
       } else {
         res.writeHead(200);
+             console.log('success');
         return res.end(data);
+        
       }
     });
   };
@@ -27,16 +31,18 @@
   io.on('connection', function(socket) {
     /* this is just for testing it needs to be compiled with actual state of each pin  this could be done via a local database*/
 
-    return fs.readFile(__dirname + 'deviceInfo/deviceInfo.js', function(err, data) {
+    return fs.readFile(__dirname + '/js/deviceInfo.js', function(err, data) {
       if (err) {
         socket.emit('error', {
           errorCode: '500',
           errorMessage: 'Failed to open Device info'
         });
+           console.log('error reading device  info');
       } else {
         /* one  option is that since we have the device info we could make a looping request to the ddevice to get its current status*/
 
         socket.emit('init', data);
+            console.log('sent device  info');
       }
       return socket.on('updateComponent', function(data) {
         /* here is where you pu in all the magic to update the network device*/
